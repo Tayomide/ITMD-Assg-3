18674cfad753a0187d08cef026027ddf11ced58f
Tayomide <70061548+Tayomide@users.noreply.github.com>
2023-11-28 18:09:54 -0600

    Last attempt at tailwind bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { AppProps } from 'next/app'
 
-// import '../styles/index.css'
-import "tailwindcss/tailwind.css";
+import '../styles/global.css'
+// import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
